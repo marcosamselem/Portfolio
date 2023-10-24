e62298fe7e2fea40ebee2b16d250e3f61d18935d
Marcos Amselem <marcosamselem27@gmail.com>
2023-10-24 19:50:15 +0200

    implemented onclicks to front end nd backend

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,29 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Skills = () => {
+  const [hide, setHide] = useState(false);
+  const handleToggleList = () => {
+    setHide(prevState => !prevState);
+  };
+
+  const [show, setShow] = useState(false);
+  const handleBackendList = () => {
+    setShow(prevState => !prevState);
+  }
+
   return(
     <main>
       <h1 className="skills-title">My Skills</h1>
       <div className="flex-d">
-        <p className="front-end">Front-end</p>
-        <p className="back-end">Back-end</p>
+        <p onClick={handleToggleList} className="front-end">Front-end</p>
+        <p onClick={handleBackendList} className="back-end">Back-end</p>
       </div>
       <div className="flex-lists">
-          <ul className="front-list">
+            {hide && <ul className="front-list">
             <li>css</li>
             <li>javascript</li>
-            <li>react.js</li>
+            <li>react</li>
             <li>3d modelling</li>
             <li>figma</li>
-            <li>stimulus.js</li>
-            <li>Vue.js</li>
+            <li>stimulus</li>
+            <li>Vue</li>
             <li>html</li>
             <li>bootstrap</li>
             <li>spline</li>
-          </ul>
-          <ul className="back-list">
+          </ul>}
+          {show && <ul className="back-list">
             <li>Typescript</li>
-            <li>Node.js</li>
+            <li>Node</li>
             <li>Ruby</li>
             <li>Ruby on Rails</li>
             <li>Python</li>
@@ -34,7 +44,7 @@ const Skills = () => {
             <li>Restful APIs</li>
             <li>Redis</li>
             <li>Ajax</li>
-          </ul>
+          </ul>}
         </div>
     </main>
   )
