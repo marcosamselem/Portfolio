6bb3dc15ec50a46590657b06a6d277adbc628211
Marcos Amselem <marcosamselem27@gmail.com>
2023-10-28 22:37:01 +0200

    navbar scrolls to sections correctly and I have started making the navbar disappear when the user scrolls down

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import "../App.css"
-import Navbar from "./Navbar"
 
 const About = () => {
   return (
-      <div>
-         <Navbar />
-         <div className="about-title">About me</div>
-         <div className="flex-items">
-            <div className="frame-paragraph">
-              <p className="myself-info">
-                I am a Full-Stack Web Developer who is passionate about making complex websites/apps
-                easy and accessible to the user. In 2021 I founded a crypto start-up where I had the chance to enhance my
-                knowledge in the Web Development field in order to build a unique and robust product that
-                would satisfy all users. I have experience in leading Web Development projects with teams
-                of full-stack developers. I consider myself to be a quick learner eager to master new coding
-                languages.
-              </p>
-            </div>
-            <img className="profile-pic" src={"/images/picture.png"} alt="" />
-          </div>
-          </div>
+    <div id="about">
+      <div className="about-title">About me</div>
+      <div className="flex-items">
+        <div className="frame-paragraph">
+          <p className="myself-info">
+            I am a Full-Stack Web Developer who is passionate about making complex websites/apps
+            easy and accessible to the user. In 2021 I founded a crypto start-up where I had the chance to enhance my
+            knowledge in the Web Development field in order to build a unique and robust product that
+            would satisfy all users. I have experience in leading Web Development projects with teams
+            of full-stack developers. I consider myself to be a quick learner eager to master new coding
+            languages.
+          </p>
+        </div>
+        <img className="profile-pic" src={"/images/picture.png"} alt="" />
+      </div>
+    </div>
+
   )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,17 +3,17 @@ import "../App.css";
 
 const Projects = (props) => {
   return (
-    <main className="card">
-      <div className="place-in-center">
-        <h1><strong>{props.title}</strong></h1>
+      <div className="card">
+        <div className="place-in-center">
+          <h1><strong>{props.title}</strong></h1>
+        </div>
+        <div className="place-in-center">
+          <img className="img-size" src={props.image} alt="" />
+        </div>
+        <h2 className="margin-card">Project length: {props.length}</h2>
+        <h2 className="margin-card">Skills used: {props.skills}</h2>
+        <h2 className="margin-card">Project description: {props.description}</h2>
       </div>
-      <div className="place-in-center">
-        <img className="img-size" src={props.image} alt="" />
-      </div>
-      <h2 className="margin-card">Project length: {props.length}</h2>
-      <h2 className="margin-card">Skills used: {props.skills}</h2>
-      <h2 className="margin-card">Project description: {props.description}</h2>
-    </main>
   );
 };
 
