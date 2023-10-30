0220a5c3e47d0afc9603332a8dbe994c16867734
Marcos Amselem <marcosamselem27@gmail.com>
2023-10-30 17:52:20 +0000

    changec all routes and applied new paths

--- src/App.js
+++ src/App.js
@@ -2,45 +2,17 @@ import './App.css';
 // import Spline from '@splinetool/react-spline';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import data from './data/projectData'
-import Footpage from './components/Footpage'
-import Navbar from "./components/Navbar"
+import Web from "./components/Web"
+
 function App() {
   return (
     <main>
-     {/* <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/web" element={<Web />} />
       </Routes>
-    </Router> */}
-     <div>
-      <Navbar />
-     <About />
-     <Skills />
-     <div id="projects">
-     <h1 className='projects-style'>My Projects</h1>
-     <div className="card-disposition">
-     {data.map(item => {
-        return(
-          <Projects
-            key={item.id}
-            title={item.title}
-            image={item.imageUrl}
-            length={item.projectLength}
-            skills={item.skillsUsed}
-            description={item.projectDescription}
-            link={item.link}
-            />
-        )
-      })
-    }
-    </div>
-      </div>
-      <Footpage />
-  </div>
+    </Router>
     </main>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect }from "react";
 import '../App.css'
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const scrollToSection = (sectionId) => {
@@ -27,7 +27,9 @@ const Navbar = () => {
 
   return (
     <nav className={hidden ? 'flex-bar hidden' : 'flex-bar'}>
+        <Link style={{textDecoration: "none"}}to="/">
         <p className="link-to">Home</p>
+        </Link>
         <p onClick={() => scrollToSection('about')}>About</p>
         <p onClick={() => scrollToSection('skills')}>Skills</p>
         <p onClick={() => scrollToSection('projects')}>Projects</p>
