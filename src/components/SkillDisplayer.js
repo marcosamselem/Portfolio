923399e9b480bd96b4330b802189a870d19bebae
Marcos Amselem <marcosamselem27@gmail.com>
2023-11-02 18:18:50 +0000

    change the design of lists of skills

---
+++ src/components/SkillDisplayer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+
+const SkillDisplayer = ({languages}) => {
+  return (
+    <div className="lists-style">
+      <ul >
+        {languages.map((language)=>{
+          return(
+            <li>{language}</li>
+          )
+        })}
+
+      </ul>
+    </div>
+  )
+}
+
+export default SkillDisplayer
