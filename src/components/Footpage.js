e6795a19f3a72ee3c69dff508c783c55356043c4
Marcos Amselem <marcosamselem27@gmail.com>
2023-11-02 15:56:55 +0000

    finished with navbar effects

--- src/components/Footpage.js
+++ src/components/Footpage.js
@@ -13,7 +13,7 @@ const Footpage = () => {
           <i class="fa-solid fa-envelope fa-xl"></i>
           <p>marcosamselem27@gmail.com</p>
         </div>
-        <a className="linkedin" style={{color: "white"}}href="https://www.linkedin.com/in/marcos-amselem-4a2026198"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+        <a className="linkedin" style={{color: "white"}} href="https://www.linkedin.com/in/marcos-amselem-4a2026198"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
         <a className="github" style={{color: "white", marginRight: "80px"}} href="https://github.com/marcosamselem"><i class="fa-brands fa-github fa-2xl"></i></a>
       </div>
   </main>
