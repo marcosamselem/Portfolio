325e0eb210ffdd25378f3a4608a94ccf6b861eaf
Marcos Amselem <marcosamselem27@gmail.com>
2023-11-13 18:29:56 +0000

    added items to the list and removed git repetition

--- src/data/programmingLanguages.js
+++ src/data/programmingLanguages.js
@@ -1,2 +1,27 @@
-export const frontEnd = ["css", "javascript", "react", "3d modelling", "figma", "stimulus", "Vue", "html", "bootstrap", "spline"]
-export const backEnd = ["Python", "Typescript", "Ruby", "Ruby on Rails", "SQL", "RESTful APIs", "Redis", "Heroku", "Git", "Git", "Ajax"]
+export const frontEnd = [
+"css",
+"javascript",
+"react",
+"3d modelling",
+"figma",
+"stimulus",
+"Vue",
+"html",
+"bootstrap",
+"spline"
+]
+
+export const backEnd = [
+  "Python",
+  "Typescript",
+  "Ruby",
+  "Ruby on Rails",
+  "SQL",
+  "MongoDB",
+  "PostgreSQL",
+  "RESTful APIs",
+  "Redis",
+  "Heroku",
+  "Git",
+  "Ajax"
+]
