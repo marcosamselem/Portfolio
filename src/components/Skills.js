e992fab3211c1e60e33eb2b6ba02643ecd6d8b6f
Marcos Amselem <marcosamselem27@gmail.com>
2023-11-13 17:18:13 +0000

    displayment list works for mobile

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SkillDisplayer from "./SkillDisplayer";
 import { frontEnd, backEnd } from "../data/programmingLanguages";
+import { useMediaQuery } from 'react-responsive';
 
 const Skills = () => {
   const [showFrontSkills, setShowFrontSkills] = useState(false);
@@ -22,20 +23,42 @@ const Skills = () => {
     setShowBackSkills(false)
   };
 
-  return (
+  const largeScreen = (
     <div className="displayment-list" id="skills">
-      <div className="skills-title">My Skills</div>
-      <div>
-        <div className="flex-d">
-          <div className="skills-list-style" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            {showFrontSkills ? <SkillDisplayer languages={frontEnd} /> : <p className="extended-list">Front-end</p>}
-          </div>
-          <div className="skills-list-style" onMouseEnter={handleMouseEnterBack} onMouseLeave={handleMouseLeaveBack}>
-            {showBackSkills ? <SkillDisplayer languages={backEnd} /> : <p className="extended-list">Back-end</p>}
-          </div>
+    <div className="skills-title">My Skills</div>
+    <div>
+      <div className="flex-d">
+        <div className="skills-list-style" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          {showFrontSkills ? <SkillDisplayer languages={frontEnd} /> : <p className="extended-list">Front-end</p>}
+        </div>
+        <div className="skills-list-style" onMouseEnter={handleMouseEnterBack} onMouseLeave={handleMouseLeaveBack}>
+          {showBackSkills ? <SkillDisplayer languages={backEnd} /> : <p className="extended-list">Back-end</p>}
         </div>
       </div>
     </div>
+  </div>
+  )
+
+  const smallScreen = (
+    <div className="displayment-list-mobile" id="skills">
+    <div className="skills-title">My Skills</div>
+    <div>
+      <div className="flex-d">
+        <div className="skills-list-style" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          {showFrontSkills ? <SkillDisplayer languages={frontEnd} /> : <p className="extended-list">Front-end</p>}
+        </div>
+        <div className="skills-list-style" onMouseEnter={handleMouseEnterBack} onMouseLeave={handleMouseLeaveBack}>
+          {showBackSkills ? <SkillDisplayer languages={backEnd} /> : <p className="extended-list">Back-end</p>}
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+
+  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
+
+  return (
+    isSmallScreen ? smallScreen : largeScreen
   );
 };
 
