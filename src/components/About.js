03dd92615ef9ac0646fd06709a365170355514e0
Marcos Amselem <marcosamselem27@gmail.com>
2024-02-05 17:11:06 +0000

    updated my cv and added new project cards

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,14 @@ const About = () => {
       <div className="flex-items">
         <div className="frame-paragraph">
           <p className="myself-info">
-            I am a Full-Stack Web Developer who is passionate about making complex websites/apps
-            easy and accessible to the user. In 2021 I founded a crypto start-up where I had the chance to enhance my
+            I am a Software Engineer passionate about making complex websites/apps
+            easy and accessible to the user. Having worked as a Software Engineer for companies such as Novatic Labs and Change The Block,
+            I have developed strong experience in Front-End, Back-End, web servers and web deployment. Moreover,
+            In 2021 I founded a Crypto start-up where I enhanced my
             knowledge in the Web Development field in order to build a unique and robust product that
             would satisfy all users. I have experience in leading Web Development projects with teams
             of full-stack developers. I consider myself to be a quick learner eager to master new coding
-            languages.
+            languages and make an impact in society through the use of technology! 🚀 🚀 🚀
           </p>
         </div>
         <img className="profile-pic" src={ProfilePic} alt="" />
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,24 +1,74 @@
 import docsightImg from "../images/docsight.png"
 import metacrewImg from "../images/metacrew.png"
+import ctbImage from "../images/ctbImage.png"
+import myAffDeal from "../images/myAffDeal.png"
+import xcape from "../images/xcape.png"
+import facturis from "../images/facturis.png"
+import rockinMedia from "../images/rockin.png"
 
 const data =  [
   {
     id: 0,
+    title: "Change The Block",
+    imageUrl: ctbImage,
+    projectLength: "1 month",
+    skillsUsed: "Python, Javascript, Typescript, Node.js, Express.js, React.js, MongoDB, Azure, Redis, PostgreSQL, HTML, CSS, SCSS, GitHub, RESTful APIs, AWS.",
+    projectDescription: "Website through which CTB manage their clients and workers and provide information to potential clients about the company's activities.",
+    link: "https://www.changetheblock.com/"
+  },
+  {
+    id: 1,
+    title: "Xcape",
+    imageUrl: xcape,
+    projectLength: "2 months",
+    skillsUsed: "Javascript, Typescript, Node.js, PHP, Express.js, React.js, Adonis.js, Azure, Redis, MySQL, Ngix, Apache, Next.js, HTML, CSS, SCSS, GitHub, RESTful APIs, AWS.",
+    projectDescription: "Travel agency where users can plan, organize and book trips from one single platforms and get the best recommendations.",
+    link: "https://xcapeonline.com/es/"
+  },
+  {
+    id: 2,
+    title: "Rockin Media",
+    imageUrl: rockinMedia,
+    projectLength: "1 month",
+    skillsUsed: "HTML, CSS, SCSS, Javascript, Typescript, Node.js, PHP, Express.js, React.js, Azure, Redis, MySQL, Next.js, GitHub, RESTful APIs, AWS.",
+    projectDescription: "Digital marketing consultancy where companies can make their business grow through the use of Rockin Media's digital strategies.",
+    link: "https://rockinmedia.es"
+  },
+  {
+    id: 3,
+    title: "Facturis",
+    imageUrl: facturis,
+    projectLength: "1.5 months",
+    skillsUsed: "React.js, Next.js, Express.js, PHP, HTML, CSS, SCSS, Tailwind CSS, Javascript, Typescript, Node.js, Express.js, Azure, Redis, PostgreSQL, GitHub, RESTful APIs, AWS.",
+    projectDescription: "Digital platform where companies can store and manage their company's invoices in a safe way with just a few clicks.",
+    link: "https://facturis.es/"
+  },
+  {
+    id: 4,
+    title: "MyAffDeal",
+    imageUrl: myAffDeal,
+    projectLength: "1 month",
+    skillsUsed: "Python, Nginx, Apache, Adonis.js, Javascript, Typescript, Node.js, Express.js, Vue.js, Nuxt.js, HTML, CSS, SCSS, React, GitHub, AWS, Azure, PostgreSQL, RESTful APIs",
+    projectDescription: "Digital platform where users can log in as admins or clients in order to make safe payments from any part of the world, using any currency.",
+    link: "https://myaffdeal.com/"
+  },
+  {
+    id: 5,
     title: "Docsight",
     imageUrl: docsightImg,
     projectLength: "2 weeks",
-    skillsUsed: "Ruby, Ruby On Rails, Javascript, Restful APIs, HTML, SCSS, SQL, Stimulus.js, Git, Git Hub, Heroku, Bootstrap.",
+    skillsUsed: "Ruby, Ruby On Rails, Javascript, Typescript, Node.js, Restful APIs, HTML, SCSS, SQL, Stimulus.js, Git, GitHub, Heroku, Bootstrap.",
     projectDescription: "Docsight is a platform where users can find their preferred doctor according to their needs and book an appointment in just a few clicks.",
     link: "https://www.docsight.me/"
   },
   {
-    id: 1,
+    id: 5,
     title: "Metacrew",
     imageUrl: metacrewImg,
     projectLength: "2 weeks",
-    skillsUsed: "HTML, CSS, React, Git Hub, Heroku.",
+    skillsUsed: "HTML, CSS, React, Javascript, Typescript, Node.js, GitHub, Heroku.",
     projectDescription: "Project420.xyz is Metacrew’s NFT community´s website where people are able to play the P2E video game and see the overview of the project.",
-    link: "https://www.project420.xyz/"
+    link: "http://www.project420.xyz/"
   }
 ]
 
